d58a7d1292083f15444dc33511ea886c78b0344d
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2018-02-24 14:09:54 +0000

    Made some changed on server and added images and README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+#**TinyApp Project
+
+TinyApp is a URL shortener web application that is designed to facillitate more efficient data commmunication and media sharing on the web
+
+##Screenshot
+
+
+
+##Project Dependencies
+
+bcrypt
+body-parser
+cookie-parser
+cookie-session
+ejs
+express
+
+##Setup Steps
+
+download the project and npm install should install all the dependencies
+add 'node express_server.js' to 'start:' in the package.json
+enter npm start in your Command Line
+then run localhost: in your browser, where PORT is the port you have set in express_server.js, note by default it is set to 8080
--- express_server.js
+++ express_server.js
@@ -3,30 +3,21 @@ const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 
-var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieSession({
-  name: 'session',
-  keys: 'thequickbrownfoxjumpsoverthelazydog'
-}));
-
-var PORT = process.env.PORT || 8080;
-
-app.set("view engine", "ejs");
-
-const usersDB = {};
+let usersDB = {};
 
 let urlsDB = {};
+
 
 function generateRandomString() {
   let randomStr = "";
   const alphabetAndDigits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     randomStr += alphabetAndDigits.charAt(Math.floor(Math.random() * alphabetAndDigits.length));
   }
   return randomStr;
 }
+
 
 function hasUser(userID) {
   if (!userID) {
@@ -37,11 +28,12 @@ function hasUser(userID) {
         return true;
       }
     }
-  return false;
+    return false;
   }
 }
 
-function hasUserEmail (email) {
+
+function hasUserEmail(email) {
   for (let user in usersDB) {
     if (usersDB[user].email === email) {
       return true;
@@ -49,6 +41,7 @@ function hasUserEmail (email) {
   }
   return false;
 }
+
 
 function getUserID(email) {
   for (let user in usersDB) {
@@ -58,36 +51,57 @@ function getUserID(email) {
   }
 }
 
-function getShortURL (url, userURLs) {
-  for (let key in userURLs) {
-    if (userURLs[key] === url) {
+
+function getShortURL(url, userID) {
+  for (let key in urlsDB[userID]) {
+    if (urlsDB[userID][key] === url) {
       return key;
     }
   }
-  return undefined;
 }
+
 
 function getLongURL(shortURL) {
-
-  for (let index in urlsDB) {
-    if (urlsDB[index].hasOwnProperty(shortURL)) {
-      return urlsDB[index][shortURL];
+  for (let key in urlsDB) {
+    if (urlsDB[key].hasOwnProperty(shortURL)) {
+      return urlsDB[key][shortURL];
+      break;
     }
   }
-  return - 1;
 }
 
-app.get('/db.json', (req, res) => {
-  res.json({ usersDB, urlsDB });
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+name: 'session',
+keys: [process.env.SECRET_KEY || 'philip']
+}));
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.set("view engine", "ejs");
+app.get("/register", (req, res) => {
+  const userID = req.session.userID;
+  if (hasUser(userID)) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_register");
+  }
 });
 
-app.get("/register", (req, res) => {
-  res.render("urls_register");
-});
 
 app.get("/login", (req, res) => {
-  res.render("urls_login");
+  const userID = req.session.userID;
+  if (hasUser(userID)) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_login");
+  }
 });
+
 
 app.get("/urls/new", (req, res) => {
   const userID = req.session.userID;
@@ -95,19 +109,25 @@ app.get("/urls/new", (req, res) => {
     const templateVars = { user: usersDB[userID]};
     res.render("urls_new", templateVars);
   } else {
-    res.redirect("http://localhost:8080/login");
+    res.redirect("/login");
   }
 });
+
 
 app.get("/urls/:id", (req, res) => {
   const userID = req.session.userID;
   if (hasUser(userID)) {
-    const templateVars = {shortURL: req.params.id, longURL: urlsDB[userID][req.params.id], user: usersDB[userID]};
-    res.render("urls_show", templateVars);
+    if (!urlsDB[userID].hasOwnProperty(req.params.id)) {
+      res.status(403).send(`shortURL not found. Create a new one (<a href="/urls_new">here</a>).`);
+    } else {
+      const templateVars = {shortURL: req.params.id, longURL: urlsDB[userID][req.params.id], user: usersDB[userID]};
+      res.render("urls_show", templateVars);
+    }
   } else {
-    res.redirect("http://localhost:8080/login");
+    res.status(403).send('User not logged in! Go to log in page (<a href="/login">Login</a>)');
   }
 });
+
 
 app.get("/urls", (req, res) => {
   const userID = req.session.userID;
@@ -115,71 +135,108 @@ app.get("/urls", (req, res) => {
     const templateVars = { user: usersDB[userID], urls: urlsDB[userID] };
     res.render("urls_index", templateVars);
   } else {
-    res.redirect("http://localhost:8080/login");
+    res.status(403).send('User not logged in! Go to <a href="/login">Login</a> or <a href="/register">Register</a> page.');
   }
 });
 
+
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = getLongURL(req.params.shortURL);
-  res.redirect(longURL);
+  let longURL = getLongURL(req.params.shortURL);
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(400).send('Requested url doesn\'t exist. Create a new one (<a href="urls_new">New shortURL</a>)');
+  }
 });
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+
 
 app.post("/register", (req, res) => {
   if(!req.body.email || !req.body.password) {
-    res.sendStatus(400);
+    res.status(400).send('Email and password required! Try again! (<a href="/register">Register</a>).');
   } else if (hasUserEmail(req.body.email)) {
-    res.sendStatus(400);
+    res.status(400).send('Email already registered! Go to login page (<a href="/login">Login</a>).');
   } else {
     const userID = generateRandomString();
     const password = req.body.password;
     const hashedPassword = bcrypt.hashSync(password, 10);
     usersDB[userID] = {"id": userID, "email": req.body.email, "password": hashedPassword};
     req.session.userID = userID;
-    res.redirect("http://localhost:8080/urls/");
+    res.redirect("/urls");
   }
 });
 
+
 app.post("/login", (req, res) => {
-  if (!hasUserEmail(req.body.email)) {
-    res.sendStatus(403);
+  if(!req.body.email || !req.body.password) {
+    res.status(400).send('Email and password required!');
+  } else if (!hasUserEmail(req.body.email)) {
+    res.status(403).send('User not registered! Register first to log in (<a href="/register">Register</a>).');
   } else if (bcrypt.compareSync(req.body.password, usersDB[getUserID(req.body.email)].password)) {
     req.session.userID = getUserID(req.body.email);
-    res.redirect("http://localhost:8080/urls");
+    res.redirect("/urls");
   } else {
-    res.sendStatus(403);
+    res.status(403).send('Invalid password! Please try again! (<a href="/login">Login</a>)');
   }
 });
+
 
 app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect("http://localhost:8080/urls/login");
+  res.redirect("/urls");
 });
+
+
 
 app.post("/urls/:id/delete", (req, res) => {
   const userID = req.session.userID;
-  delete urlsDB[userID][req.params.id];
-  res.redirect("http://localhost:8080/urls/");
+  if (hasUser(userID)) {
+    if (urlsDB[userID].hasOwnProperty(req.params.id)) {
+      delete urlsDB[userID][req.params.id];
+      res.redirect("/urls/");
+    } else {
+      res.status(403).send(`shortURL not found. Create a new one (<a href="/urls_new">here</a>)`);
+    }
+  } else {
+    res.status(403).send('User not logged in! Log in first! (<a href="/login">Login</a>)');
+  }
 });
 
+
+
 app.post("/urls/:id", (req, res) => {
-  urlDataBase[req.params.id] = req.params.longURL;
-  res.redirect("http://localhost:8080/urls/");
+  const userID = req.session.userID;
+  if (hasUser(userID)) {
+    if (urlsDB[userID].hasOwnProperty(req.params.id)) {
+      urlsDB[userID][req.params.id] = req.body.longURL;
+      res.redirect('/urls/' + req.params.id);
+    } else {
+      res.status(403).send(`shortURL not found. Create a new one (<a href="/urls_new">here</a>)`);
+    }
+  } else {
+    res.redirect("/urls/");
+  }
 });
+
 
 app.post("/urls", (req, res) => {
   const userID = req.session.userID;
-  const shortURL = generateRandomString();
-  const userURLs = urlsDB[userID] || {};
-  const oldShortURL = getShortURL(req.body.longURL, userURLs);
-  if (oldShortURL) {
-    delete userURLs[oldShortURL];
+  if (hasUser(userID)) {
+    const userURLs = urlsDB[userID] || {};
+    const shortURL = generateRandomString();
     userURLs[shortURL] = req.body.longURL;
+    urlsDB[userID] = userURLs;
+
+    res.redirect('/urls/' + shortURL);
   } else {
-    userURLs[shortURL] = req.body.longURL;
+    res.status(403).send('User not logged in! Log in first! (<a href="/login">Login</a>)');
   }
-  urlsDB[userID] = userURLs;
-  res.redirect('http://localhost:8080/urls/' + shortURL);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
